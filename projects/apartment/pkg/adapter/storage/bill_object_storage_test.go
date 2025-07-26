a6675e333e8d60ec50ee812ba82171e7c98fc7f3
momoein <momoein711@gmail.com>
2025-07-26 17:45:06 +0330

    test: add test for bill object storage adapter

---
+++ projects/apartment/pkg/adapter/storage/bill_object_storage_test.go
@@ -0,0 +1,33 @@
+package storage
+
+import (
+	"context"
+	"testing"
+
+	appctx "github.com/arcaptcha-internship-2025/momoein-apartment/pkg/context"
+	"github.com/arcaptcha-internship-2025/momoein-apartment/pkg/logger"
+	"github.com/arcaptcha-internship-2025/momoein-apartment/pkg/minio"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	log = logger.NewConsoleZapLogger(logger.ModeDevelopment)
+	ctx = appctx.New(context.Background(), appctx.WithLogger(log))
+)
+
+func TestBillObjectStorage_FPut(t *testing.T) {
+	client := minio.MustNewClient("localhost:9000", "minioadmin", "minioadmin")
+	obj := NewBillObjectStorage(client)
+
+	err := obj.FPut(ctx, "test-2", "../../../test/data/my-testfile")
+	assert.Nil(t, err)
+}
+
+func TestBillObjectStorage_FGet(t *testing.T) {
+	client := minio.MustNewClient("localhost:9000", "minioadmin", "minioadmin")
+
+	obj := NewBillObjectStorage(client)
+
+	err := obj.FGet(ctx, "test-1", "../../../test/data/my-testfile1")
+	assert.Nil(t, err)
+}
