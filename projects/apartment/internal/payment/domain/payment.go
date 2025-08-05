c35221c209101c2a917db72f0a6f74f3450bca54
momoein <momoein711@gmail.com>
2025-08-05 12:59:02 +0330

    feat: update payment domain

--- projects/apartment/internal/payment/domain/payment.go
+++ projects/apartment/internal/payment/domain/payment.go
@@ -24,13 +24,30 @@ type Payment struct {
 	UpdatedAt time.Time  `json:"updated_at"`
 	DeletedAt *time.Time `json:"deleted_at,omitempty"`
 
-	BillID      uuid.UUID `json:"bill_id"`
-	PayerID     uuid.UUID `json:"payer_id"`
-	Amount      int64     `json:"amount"` // in smallest currency unit, e.g., Rial or Toman
-	PaymentDate time.Time `json:"payment_date"`
+	BillID  uuid.UUID `json:"bill_id"`
+	PayerID uuid.UUID `json:"payer_id"`
+	Amount  int64     `json:"amount"`
+	PaidAt  time.Time `json:"payment_date"`
 
 	Status        PaymentStatus `json:"status"`
 	Gateway       string        `json:"gateway"`
 	TransactionID string        `json:"transaction_id,omitempty"`
 	CallbackData  CallbackData  `json:"callback_data,omitempty"` // Use map for parsed JSONB
+}
+
+type GatewayType string
+
+func (g GatewayType) String() string {
+	return string(g)
+}
+
+const MockGateway = "mock-gateway"
+
+var validGateways = map[GatewayType]struct{}{
+	MockGateway: {},
+}
+
+func (g GatewayType) IsValid() bool {
+	_, ok := validGateways[g]
+	return ok
 }
