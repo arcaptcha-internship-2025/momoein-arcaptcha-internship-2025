034f475a25d90f070bceeb5652a5b5a2d5784196
momoein <momoein711@gmail.com>
2025-08-05 13:00:56 +0330

    feat: update payment port

--- projects/apartment/internal/payment/port/payment.go
+++ projects/apartment/internal/payment/port/payment.go
@@ -3,10 +3,23 @@ package port
 import (
 	"context"
 
+	"github.com/arcaptcha-internship-2025/momoein-apartment/internal/common"
 	"github.com/arcaptcha-internship-2025/momoein-apartment/internal/payment/domain"
 )
 
-type PaymentService interface {
-	CreatePayment(ctx context.Context, billID string, gateway string) (*domain.Payment, error)
-	HandleCallback(ctx context.Context, gateway string, data map[string]string) error
+type Service interface {
+	PayBill(ctx context.Context, gateway domain.GatewayType, billID, userID common.ID) (redirectURL string, err error)
+	PayTotalDebt(ctx context.Context, gateway domain.GatewayType, userID common.ID) (redirectURL string, err error)
+	HandleCallback(ctx context.Context, gateway domain.GatewayType, data map[string]string) error
+}
+
+type Repo interface {
+	CreatePayment(ctx context.Context, p *domain.Payment) (*domain.Payment, error)
+	UpdateStatus(ctx context.Context, paymentID common.ID, s domain.PaymentStatus) error
+	UserBillBalanceDue(ctx context.Context, userId, billId common.ID) (int64, error)
+}
+
+type Gateway interface {
+	CreateTransaction(ctx context.Context, payment *domain.Payment) (redirectURL string, err error)
+	VerifyTransaction(ctx context.Context, data map[string]string) (bool, error)
 }
