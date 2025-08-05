ddf035b6f8fdfbc0616b9f7861a225ff109fe868
momoein <momoein711@gmail.com>
2025-08-05 13:18:13 +0330

    refactor: simplify Run function and move route logic in RegisterAPI function

--- projects/apartment/api/handler/setup.go
+++ projects/apartment/api/handler/setup.go
@@ -11,14 +11,7 @@ import (
 
 func Run(app app.App) error {
 	r := router.NewRouter()
-	r.Use(
-		middleware.SetRequestContext(app),
-		middleware.LogRequest(),
-	)
-	r.Get("/", getRootHandler())
-
-	api := r.Group("/api/v1", nil)
-	RegisterAPI(api, app)
+	RegisterAPI(r, app)
 
 	addr := fmt.Sprintf(":%d", app.Config().HTTP.Port)
 	app.Logger().Info("listen on " + addr)
@@ -26,35 +19,47 @@ func Run(app app.App) error {
 }
 
 func RegisterAPI(r *router.Router, app app.App) {
-	secret := []byte(app.Config().Auth.JWTSecret)
+	jwtSecret := []byte(app.Config().Auth.JWTSecret)
 
 	usrSvcGetter := UserServiceGetter(app)
 	bilSvcGtr := BillServiceGetter(app)
 	aptSvcGetter := ApartmentServiceGetter(app)
 
-	r.Group("/auth", func(r *router.Router) {
-		r.Post("/sign-up", getSignUpHandler(usrSvcGetter, app.Config().Auth))
-		r.Get("/sign-in", getSignInHandler(usrSvcGetter, app.Config().Auth))
-		r.Get("/refresh-token", RefreshTokenHandler(usrSvcGetter, app.Config().Auth))
-	})
+	r.Use(
+		middleware.SetRequestContext(app),
+		middleware.LogRequest(),
+	)
+	r.Get("/", getRootHandler())
 
-	r.Group("/apartment", func(r *router.Router) {
-		r.Use(middleware.NewAuth(secret))
+	r.Group("/api/v1", func(r *router.Router) {
 
-		acceptURL := app.Config().BaseURL + "/api/v1/apartment/invite/accept"
-		r.Post("/", AddApartment(aptSvcGetter))
-		r.Post("/invite", InviteApartmentMember(aptSvcGetter, acceptURL))
-		r.Get("/invite/accept", AcceptApartmentInvite(aptSvcGetter))
+		r.Group("/auth", func(r *router.Router) {
+			r.Post("/sign-up", getSignUpHandler(usrSvcGetter, app.Config().Auth))
+			r.Get("/sign-in", getSignInHandler(usrSvcGetter, app.Config().Auth))
+			r.Get("/refresh-token", RefreshTokenHandler(usrSvcGetter, app.Config().Auth))
+		})
 
-		r.Post("/bill", AddBill(bilSvcGtr))
-		r.Get("/bill", GetBill(bilSvcGtr))
-		r.Get("/bill/image", GetBillImage(bilSvcGtr))
-	})
+		r.Group("/apartment", func(r *router.Router) {
+			r.Use(middleware.NewAuth(jwtSecret))
+			acceptURL := app.Config().BaseURL + "/api/v1/apartment/invite/accept"
 
-	r.Group("/user", func(r *router.Router) {
-		r.Use(middleware.NewAuth(secret))
-		r.Get("/total-debt", GetUserTotalDept(bilSvcGtr))
-		r.Get("/bill-shares", GetUserBillShares(bilSvcGtr))
+			r.Post("/", AddApartment(aptSvcGetter))
+			r.Post("/invite", InviteApartmentMember(aptSvcGetter, acceptURL))
+			r.Get("/invite/accept", AcceptApartmentInvite(aptSvcGetter))
+		})
+
+		r.Group("/bill", func(r *router.Router) {
+			r.Post("/", AddBill(bilSvcGtr))
+			r.Get("/", GetBill(bilSvcGtr))
+			r.Get("/image", GetBillImage(bilSvcGtr))
+		})
+
+		r.Group("/user", func(r *router.Router) {
+			r.Use(middleware.NewAuth(jwtSecret))
+
+			r.Get("/total-debt", GetUserTotalDept(bilSvcGtr))
+			r.Get("/bill-shares", GetUserBillShares(bilSvcGtr))
+		})
 	})
 }
 
